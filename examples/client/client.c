a3f63c7bda74a600c4d850c4383ccb5688782156
Takashi Kojo <kojo@wolfssl.com>
2023-10-17 12:16:21 +0900

    fix const str size in client.c

--- examples/client/client.c
+++ examples/client/client.c
@@ -2925,7 +2925,7 @@ THREAD_RETURN WOLFSSL_THREAD client_test(void* args)
 
     if (sendGET) {
         printf("SSL connect ok, sending GET...\n");
-        msgSz = 29;
+        msgSz = 28;
         strncpy(msg, "GET /index.html HTTP/1.0\r\n\r\n", msgSz);
         msg[msgSz] = '\0';
 
