e444d96f8819dd0d4c36ea5deae5a81d6e36e801
Takashi Kojo <kojo@wolfssl.com>
2025-07-04 14:55:46 +0900

    Add wolfCrypt version

---
+++ wolfcrypt/src/selftest.c
@@ -0,0 +1,27 @@
+/* selftest.c
+ *
+ * Copyright (C) 2006-2020 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL.
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
+#if defined(HAVE_SELFTEST) || defined(HAVE_GETVERSION)
+WOLFSSL_API const char *wolfCrypt_GetVersion_CAVP_selftest(void)
+{
+    return "wolfCrypt v4.7.0i";
+}
+#endif
